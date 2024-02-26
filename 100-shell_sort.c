ae651f02bb3147ac337265de22211a499d037cf2
Y-admen <yassminash305@gmail.com>
2024-02-26 09:38:36 +0200

    ll

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -29,9 +29,9 @@ int calc_gap(size_t size)
 	n = 1;
 	while (n < size)
 	{
-		n = n * 3 + 1;
+		n = n * 3 + 1;/*next gap*/
 	}
-	return ((n - 1) / 3);
+	return ((n - 1) / 3);/*back gap*/
 
 }
 /**
@@ -48,7 +48,7 @@ void shell_sort(int *array, size_t size)
 		return;
 
 
-	for (gap = calc_gap(size); gap > 0; gap /= 2)
+	for (gap = calc_gap(size); gap > 0; gap /= 2)/*gap loop back gap*/
 	{
 		for (i = gap; i < size; i++)
 		{
